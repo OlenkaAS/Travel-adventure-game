64dc17fc5fa1c180649148ce88c0be940064f6f8
OlenkaAS <60243842+OlenkaAS@users.noreply.github.com>
2021-02-01 21:43:49 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,55 @@
+/* 
+ * File: main.cpp
+ * Author: Olga Seymour
+ * Date: 11/03/2020
+ */
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include "prompt.h"
+#include "response.h"
+
+using namespace std;
+
+int main()
+{
+    ifstream in;  //input file variable
+    
+    //Creates a vector of Prompt objects
+    vector<Prompt> prompt;
+    
+    //Makes Prompt object
+    Prompt p = Prompt();
+    
+    //Opens the game input file
+    in.open("Game.txt"); 
+      
+    //Uses current time as seed for random generator 
+    srand(time(0)); 
+    
+    //Reads game input file into the vector of Prompt objects
+    in >> p; 
+    while(!in.eof()){  
+        prompt.push_back(p);
+        in >> p; 
+    }
+    //Plays the game until the user is done 
+    int choice = 0;  
+    cout << prompt[choice];   //prints out the first prompt
+    do{ 
+        //User's response to get the next prompt index
+        int indexResp;       //index into the vector of Response objects.   
+        cin >> indexResp;
+      
+        //Gets the next prompt index based on user's response 
+        choice = prompt[choice].getResponse(indexResp).getIndex();  
+            if( choice != -1){
+            cout << prompt[choice];   
+            }                                                
+    }
+    while( choice != -1);
+    
+    cout << "Thank you for playing our traveling adventure game!" << endl;
+    return 0;
+}
